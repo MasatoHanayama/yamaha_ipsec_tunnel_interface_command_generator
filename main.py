c9f3c80d2a90b29d96ec4ba9786218c66d6093ad
Masato Hanayama <m.hanayama@gmail.com>
2020-04-17 07:33:04 +0900

    Initial commit.

---
+++ main.py
@@ -0,0 +1,21 @@
+#!/bin/bash
+#-*- encoding: utf-8 -*-
+
+# そのうち機能追加予定
+# import argparse
+
+with open('./conf.txt', 'w') as f:
+    for i in range(1, 101, 1):
+        f.write('tunnel select ' + str(i) + '\n')
+        f.write('tunnel capsulation l2tp\n')
+        f.write('ipsec tunnel ' + str(i) + '\n')
+        f.write(' ipsec sa policy ' + str(i) + ' ' + str(i) + 'esp 3es-cbc shs-hmac\n')
+        f.write(' ipsec ike keepalive log ' + str(i) + ' off\n')
+        f.write(' ipsec ike keepalive use ' + str(i) + ' off\n')
+        f.write(' ipsec ike nat-traversal ' + str(i) + ' on\n')
+        f.write(' ipsec ike pre-shared-key ' + str(i) + ' text key-phrase\n')
+        f.write(' ipsec ike remote address ' + str(i) + ' any\n')
+        f.write('l2tp tunnel auth off\n')
+        f.write('l2tp tunnel disconnect time off\n')
+        f.write('ip tunnel tcp mss limit auto\n')
+        f.write('tunnel enable ' + str(i) + '\n')
